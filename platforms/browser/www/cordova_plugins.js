21a1c6e0e86462d5132b134b09a9100b8f86e671
PierreOSX <pierre@pierre.kamar.nz>
2018-03-28 12:59:53 +1300

    Unpacked APK

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,33 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "id": "cordova-plugin-dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "phonegap-plugin-push": "1.6.0",
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-dialogs": "1.2.1"
+};
+// BOTTOM OF METADATA
+});
